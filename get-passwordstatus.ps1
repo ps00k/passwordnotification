f14234f0d71771500903f4b936499686fcd80595
amitai <dan@brandtlautman.com>
2017-03-21 09:17:19 -0400

    adding password status checker
    Added script: Script pulls all enabled AD users, sorts by date password was last set, and exports to CSV.

---
+++ get-passwordstatus.ps1
@@ -0,0 +1,3 @@
+# Script pulls all enabled AD users, sorts by date password was last set, and exports to CSV.
+
+Get-ADUser -filter 'enabled -eq $true' -properties passwordlastset, passwordneverexpires, msDS-UserPasswordExpiryTimeComputed | sort-object passwordlastset | select-object Name, passwordlastset, passwordneverexpires, @{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | Export-csv -path c:\stage\passwordreport.csv
